cc323f3841291eaf93ec7e7bd8980dc821ccbb99
ShadowZer0 <jellyfinhub@gmail.com>
2026-03-20 10:43:54 +0100

    Update and rename Screencast Button to Screencast Button.js

---
+++ Screencast Button.js
@@ -0,0 +1,8 @@
+const castObserver = new MutationObserver(function() {
+    const castBtn = document.querySelector('.headerCastButton');
+    if (castBtn) {
+        castBtn.style.setProperty('display', 'flex', 'important');
+    }
+});
+castObserver.observe(document.body, { childList: true, subtree: true, attributes: true });
+```
